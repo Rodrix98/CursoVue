9d425d9eecdf306f68f3e32c55400c0574336e94
rgarcia-netbel <146452577+rgarcia-netbel@users.noreply.github.com>
2023-12-05 19:05:43 -0300

    Add files via upload

---
+++ composition-19-vuex-starting-project/src/components/ControlCenter.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="inc">Increment</button>
+</template>
+
+<script>
+import {} from 'vuex';
+
+export default {
+  setup() {
+    const store = useStore();
+
+    function inc() {
+      store.dispatch('increment');
+    }
+
+    return { inc };
+  },
+};
+</script>
