d5f86432dc4b7052e5ef517e9780c0e2463fc657
rgarcia-netbel <146452577+rgarcia-netbel@users.noreply.github.com>
2023-12-05 19:04:00 -0300

    Add files via upload

---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <input type="text" placeholder="your name" v-model="enteredName">
+            <input type="text" placeholder="your age" v-model="enteredAge">
+            <button>Set UserData</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    emits: ['set-data'],
+    data() {
+        return {
+            enteredName: '',
+            enteredAge: '',
+        }
+    },
+    methods: {
+        submitData() {
+            this.$emit('set-data', this.enteredName, this.enteredAge);
+        }
+    }
+}
+</script>
+
